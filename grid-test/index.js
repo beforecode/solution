50348bcdae081eaebe6b410282ae3f7e34875d76
beforecode <yassineuniversite@gmail.coms>
2022-02-23 17:22:06 +0100

    the sollution is using getAttribute()

--- grid-test/index.js
+++ grid-test/index.js
@@ -1,9 +1,26 @@
-let srcShow = document.getElementById("imgClickedAndChange").src;
+//i've changed srcShow.src to srcShow
+const srcShow = document.getElementById("imgClickedAndChange");
 const imgs = document.querySelectorAll(".showcase");
 
+
 imgs.forEach(function (img) {
-	img.addEventListener("click", function () {
-		srcShow = img.src;
-		console.log(srcShow);
+	img.addEventListener("click", function (e) {
+		let srcAttr = e.target.getAttribute("src")
+		srcShow.src = srcAttr
 	});
 });
+
+
+// this is how i would write the solution, in ES6 syntax
+// not 1: both ways work the same
+// not 2: i gess the lag that is happening when switching pics is due to high reselotion of the pics
+
+// imgs.forEach( img => {
+// 	img.addEventListener("click", e => {
+// 		updateHomeImg(e)
+// 	});
+// });
+// const updateHomeImg = e => {
+// 	let srcAttr = e.target.getAttribute("src")
+// 	srcShow.src = srcAttr
+// }
